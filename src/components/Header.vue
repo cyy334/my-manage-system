4e6d8f926b088490645e98382cd2e3fb694b9f1d
cyy334 <295711590@qq.com>
2019-05-29 10:26:57 +0800

    my-manage-system

---
+++ src/components/Header.vue
@@ -0,0 +1,91 @@
+<template>
+<div class="header">
+    <div class="menu-btn">
+        <i class="el-icon-menu"></i>
+    </div>
+    <div class="logo">后台管理系统</div>
+    <div class="user-info">
+        <div class="full-screen">
+            <i class="el-icon-rank"></i>
+        </div>
+        <div class="remind">
+            <i class="el-icon-bell"></i>
+        </div>
+        <div class="user-avator">
+            <img src="../assets/images/avator.jpg">
+            </div>
+                <el-dropdown class="user-name" trigger="click">
+                    <span class="el-dropdown-link">下拉菜单<i class="el-icon-arrow-down el-icon--right"></i></span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>黄金糕</el-dropdown-item>
+                        <el-dropdown-item>狮子头</el-dropdown-item>
+                        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                        <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+                        <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header {
+    height: 70px;
+    background-color: #242f42;
+    padding: 0 30px;
+    color: #fff;
+}
+
+.header .menu-btn {
+    float: left;
+}
+
+.header .menu-btn i {
+    font-size: 30px;
+    line-height: 70px;
+}
+
+.header .logo {
+    float: left;
+    line-height: 70px;
+    font-size: 22px;
+    margin-left: 20px;
+}
+
+.header .user-info {
+    float: right;
+    font-size: 22px;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.header .user-info>div {
+    float: left;
+    margin-left: 20px;
+}
+
+.header .user-avator {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    overflow: hidden;
+}
+
+.header .user-avator img {
+    width: 100%;
+}
+
+.header .user-name {
+    font-size: 16px;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+   <div>
+    <v-head></v-head>
+    <v-sidebar></v-sidebar>
+   </div>
+</template>
+
+<script>
+import vHead from './Header.vue';
+import vSidebar from './SliderBar.vue';
+export default {
+  name: 'HelloWorld',
+  components: {
+    vHead,
+    vSidebar
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
